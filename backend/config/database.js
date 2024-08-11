6716c1ffb6aa442725c1594647b31b942f57c805
Rohit Kumar Chaurasia <147132630+Rohit8953@users.noreply.github.com>
2024-08-11 01:57:28 -0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,9 @@ const mongoose=require('mongoose');
 require('dotenv').config();
 
   const connection=()=>{
-    mongoose.connect(process.env.DATABASE_URL)
+    mongoose.connect(process.env.DATABASE_URL,{
+      connectTimeoutMS: 10000, 
+    })
     .then(()=>{console.log("db connection successfyll----")})
     .catch((err)=>{
           console.log("db connection unsuccessful",err);
